8fcfdf19deda15555764d0ce656ad6887b91c9f1
Quoc Khanh <khanhquoc2920@gmail.com>
2021-09-23 15:31:30 +0700

    buoi5

---
+++ ex7.py
@@ -0,0 +1,15 @@
+for x in range(5):
+    print(x)
+    if x==2 : break
+else :
+    print("vong lap ket thuc tai x = {}".format(x))
+
+
+# dssv = ["an,khanh,cu,de,em"] 
+# for sinhvien in dssv:
+#     print(sinhvien)
+
+# tong = 0 
+# for i in range(11):
+#     tong = tong + i
+# print(f"tong tu 0 toi 10 la {tong}")
